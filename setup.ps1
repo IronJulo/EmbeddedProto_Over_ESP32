513932766402d5d10b0babb2a4c73f51d3f9002a
IronJulo <jules.brss@gmail.com>
2023-11-17 23:06:28 +0100

    feat: add setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+# Setup EmbeddedProto
+cd EmbeddedProto
+python setup.py
+cd ..
+
+# Setup ESP32_Client
+protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\ESP32_Client\src\protocol protocol.proto
+cp .\EmbeddedProto\src\* .\ESP32_Client\src\embedded_proto
+
+# Setup Backend
+cd Backend
+npm i
+npm run server
