699d028e2e53583d6b2fb2b1853ce5af5e47b0ab
IronJulo <jules.brss@gmail.com>
2023-11-19 21:17:25 +0100

    feat: add secret.hpp.example

--- ESP32_Client/src/main.cpp
+++ ESP32_Client/src/main.cpp
@@ -1,18 +1,112 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
+#include "secret.hpp"
+#include "protocol.h"
+#include <WriteBufferFixedSize.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int deviceId = 123456789;
+
+WiFiClient client;
+
+EmbeddedProto::WriteBufferFixedSize<256> writeBuffer;
+
+test::device deviceData;
+test::ping pingData;
+
+void connectToWifi()
+{
+	Serial.print("Connecting to SSID: ");
+	Serial.println(SECRET_SSID);
+
+	auto status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+
+	Serial.print("Waiting");
+	while (status != WL_CONNECTED)
+	{
+		Serial.print(".");
+	}
+	Serial.println("You're connected to the network.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+bool pingServer()
+{
+	deviceData.set_device_serial_id(deviceId);
+	pingData.set_device_data(deviceData);
+
+	const auto serialize_result = pingData.serialize(writeBuffer);
+
+	bool result = false;
+	if (client.connect(SERVER_IP, SERVER_PORT))
+	{
+		Serial.println("Sending N bytes to the server: " + String(writeBuffer.get_size()));
+		for (int i = 0; i < writeBuffer.get_size(); ++i)
+		{
+			const byte b = *(writeBuffer.get_data() + i);
+			Serial.print(b);
+			Serial.print(" ");
+		}
+		Serial.println();
+
+		Serial.println("POST /api/data HTTP/1.1");
+		Serial.println("Host: " + String(SERVER_IP) + "/api/data");
+		Serial.println("Content-Type: application/x-protobuf");
+		Serial.println("Connection: close");
+		Serial.println("Transfer-Encoding: chunked");
+		Serial.println("");
+		Serial.println(writeBuffer.get_size(), HEX);
+		Serial.write(writeBuffer.get_data(), writeBuffer.get_size());
+		Serial.println();
+		Serial.println('0');
+		Serial.println();
+
+		client.println("POST /api/data HTTP/1.1");
+		client.println("Host: " + String(SERVER_IP) + "/api/data");
+		client.println("Content-Type: application/x-protobuf");
+		client.println("Connection: close");
+		client.println("Transfer-Encoding: chunked");
+		client.println("");
+		client.println(writeBuffer.get_size(), HEX);
+		client.write(writeBuffer.get_data(), writeBuffer.get_size());
+		client.println();
+		client.println('0');
+		client.println();
+
+		// Next wait for the response.
+		Serial.println();
+
+		delay(500);
+
+		while (client.available())
+		{
+			String string = client.readStringUntil('\n');
+			Serial.println(string);
+			if (string.startsWith(""))
+			{
+				result = true;
+			}
+		}
+	}
+	else
+	{
+		Serial.println("Failed to connect to server.");
+	}
+
+	client.stop();
+	return result;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+	Serial.begin(9600);
+	connectToWifi();
+	pingServer();
+	Serial.println("Waiting...");
+}
+
+void loop()
+{
+	pingServer();
+	delay(500);
+
 }
