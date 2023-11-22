c6037b19ad193346a45d363d00073b7de2350242
IronJulo <jules.brss@gmail.com>
2023-11-22 23:05:01 +0100

    chore: cleanup index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const protobuf = require("protobufjs");
 const app = express();
+
 const port = 3000; // You can choose any available port
 const hostname = '0.0.0.0';
 
@@ -14,28 +15,25 @@ protobuf.load("../Protocol/protocol.proto", function (err, root) {
 	var Data = root.lookupType("test.data");
 	var Ping = root.lookupType("test.ping");
 
+	// Define a route
+	app.post('/api/data', (req, res) => {
+		console.log("Data on /api/data");
+		let receive_buffer = Buffer.alloc(50000);
 
+		//let message = Data.decode(req.body);
+		console.log('req.body: ');
+		console.log(req.body);
+		res.sendStatus(200)
+	});
 
+	// Define a route
+	app.get('/', (req, res) => {
+		res.send('Hello, World!');
+	});
 
-// Define a route
-app.post('/api/data', (req, res) => {
-	console.log("Data on /api/data");
-	let receive_buffer = Buffer.alloc(50000);
-
-	//let message = Data.decode(req.body);
-	console.log('req.body: ');
-	console.log(req.body);
-	res.sendStatus(200)
-});
-
-// Define a route
-app.get('/', (req, res) => {
-	res.send('Hello, World!');
-});
-
-// Start the server
-app.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+	// Start the server
+	app.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+	});
 
 });
