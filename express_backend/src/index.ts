d2e29ce3c1cfed22425f7f40c9f3ad6fe12f9d0b
IronJulo <jules.brss@gmail.com>
2023-11-25 23:14:46 +0100

    feat: added checksum and packet type

--- express_backend/src/index.ts
+++ express_backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { basic } from './generated/basic';  // Import from otherFile.ts
+import { test } from './generated/protocol';  // Import from otherFile.ts
 import * as fs from 'fs';
+import { error } from 'console';
 
 const app = express();
 const port = 3000;
@@ -19,10 +21,48 @@ app.post('/api/device', (req, res) => {
         const deserializedDevice = basic.device.deserializeBinary(req.body);
         console.log(deserializedDevice.device_serial_id);
         res.status(200).send('Object recreated successfully');
-      } catch (error) {
+    } catch (error) {
         console.error('Error processing binary data:', error);
         res.status(500).send('Error processing binary data');
-      }
+    }
+});
+
+app.post('/api/test_device', (req, res) => {
+    console.log('POST on: /api/test_device');
+    try {
+        const packetType = req.header('Packet-Type');
+        const headerChecksum = Number(req.get('Packet-Checksum')) ?? 0;
+        console.log("packetType: " + packetType);
+        console.log("headerChecksum: " + headerChecksum);
+
+        switch (packetType) {
+            case "1":
+                const serializedPayload: Uint8Array = req.body;
+                let checksum = 0;
+                for (let index = 0; index < serializedPayload.length; index++) {
+                    const element = serializedPayload[index];
+                    checksum += element;
+                }
+                console.log("checksum: " + checksum);
+                
+                if (checksum != headerChecksum)
+                {
+                    throw error("checksum != headerChecksum");
+                }
+                const deserializedPayload = test.payload.deserializeBinary(serializedPayload);
+                console.log(deserializedPayload.toString());
+                break;
+        
+            default:
+                console.log('Unknown packet type');
+                break;
+        }
+        
+        res.status(200).send('Object recreated successfully');
+    } catch (error) {
+        console.error('Error processing binary data:', error);
+        res.status(500).send('Error processing binary data');
+    }
 });
 
 app.listen(port, () => {
@@ -35,7 +75,36 @@ const myDevice = new basic.device({
 });
 
 const binaryData: Uint8Array = myDevice.serializeBinary();
-const filePath = 'output.bin'; // You can change the file name and path as needed
 
-// Write binary data to the file
-fs.writeFileSync(filePath, binaryData);
+let binaryDataChecksum = 0;
+for (let index = 0; index < binaryData.length; index++) {
+    const element = binaryData[index];
+    binaryDataChecksum += element;
+}
+console.log("binaryDataChecksum: ");
+console.log(binaryDataChecksum);
+
+fs.writeFileSync('output_basic_device_12345.bin', binaryData);
+
+const mtTestDevice = new test.device({
+    device_serial_id: 700,
+})
+
+const myPayload = new test.payload({
+    device_data: mtTestDevice,
+    latitude: 50,
+    longitude: 3,
+    pitch: 4,
+    roll: 5,
+    yaw: 6,
+})
+
+const binaryData2: Uint8Array = myPayload.serializeBinary();
+let binaryData2Checksum = 0;
+for (let index = 0; index < binaryData2.length; index++) {
+    const element = binaryData2[index];
+    binaryData2Checksum += element;
+}
+console.log("binaryData2Checksum: ");
+console.log(binaryData2Checksum);
+fs.writeFileSync('output_test_payload_700_50_3_4_5_6.bin', binaryData2);
--- setup.ps1
+++ setup.ps1
@@ -7,7 +7,7 @@ cd ..
 protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\ESP32_Client\src\protocol protocol.proto
 
 # Setup express_backend
-# protoc -I .\Protocol\ --ts_out=.\express_backend\generated\ protocol.proto
+# protoc -I .\Protocol\ --ts_out=.\express_backend\src\generated protocol.proto
 protoc -I .\Protocol --ts_out=.\express_backend\src\generated basic.proto
 
 cp .\EmbeddedProto\src\* .\ESP32_Client\src\embedded_proto
