353dfc10dfa7404e8072b92a313f3300a54be7d8
IronJulo <jules.brss@gmail.com>
2023-11-25 14:47:00 +0100

    feat: add working ts protobuf

--- express_backend/src/index.ts
+++ express_backend/src/index.ts
@@ -1,13 +1,41 @@
 import express from 'express';
-import './generated/protocol';  // Import from otherFile.ts
+import bodyParser from 'body-parser';
+import { basic } from './generated/basic';  // Import from otherFile.ts
+import * as fs from 'fs';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.raw({ type: 'application/x-protobuf' }));
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
+    res.send('Hello, Express with TypeScript!');
+});
+
+
+app.post('/api/device', (req, res) => {
+    console.log('POST on: /api/device');
+    try {
+        const deserializedDevice = basic.device.deserializeBinary(req.body);
+        console.log(deserializedDevice.device_serial_id);
+        res.status(200).send('Object recreated successfully');
+      } catch (error) {
+        console.error('Error processing binary data:', error);
+        res.status(500).send('Error processing binary data');
+      }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+const myDevice = new basic.device({
+    device_serial_id: 12345,
+});
+
+const binaryData: Uint8Array = myDevice.serializeBinary();
+const filePath = 'output.bin'; // You can change the file name and path as needed
+
+// Write binary data to the file
+fs.writeFileSync(filePath, binaryData);
--- setup.ps1
+++ setup.ps1
@@ -5,8 +5,10 @@ cd ..
 
 # Setup ESP32_Client
 protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\ESP32_Client\src\protocol protocol.proto
-# npm install -g protoc-gen-ts
-protoc -I .\Protocol\ --ts_out=.\express_backend\generated\ protocol.proto
+
+# Setup express_backend
+# protoc -I .\Protocol\ --ts_out=.\express_backend\generated\ protocol.proto
+protoc -I .\Protocol --ts_out=.\express_backend\src\generated basic.proto
 
 cp .\EmbeddedProto\src\* .\ESP32_Client\src\embedded_proto
 
