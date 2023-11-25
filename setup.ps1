12522c0791652c898bd03164ea226f8dc702db53
IronJulo <jules.brss@gmail.com>
2023-11-25 10:53:42 +0100

    feat: add "clean" express backend
    trying with ts

--- setup.ps1
+++ setup.ps1
@@ -5,9 +5,13 @@ cd ..
 
 # Setup ESP32_Client
 protoc --plugin=protoc-gen-eams=.\EmbeddedProto\protoc-gen-eams.bat -I .\Protocol\ --eams_out=.\ESP32_Client\src\protocol protocol.proto
+# npm install -g protoc-gen-ts
+protoc -I .\Protocol\ --ts_out=.\express_backend\generated\ protocol.proto
+
 cp .\EmbeddedProto\src\* .\ESP32_Client\src\embedded_proto
 
 # Setup Backend
 cd Backend
 npm i
 npm run server
+
